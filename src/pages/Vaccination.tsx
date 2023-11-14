2f3c52bfa0017cec50f34f2768fdf08d515b5061
msyahrulsp <mssp892@gmail.com>
2023-11-14 16:45:54 +0700

    feat:

--- src/pages/Vaccination.tsx
+++ src/pages/Vaccination.tsx
@@ -1,9 +1,28 @@
 import { useEffect, useState } from 'react';
 import { Layout } from '../components/Layout';
-import { Box, useToast } from '@chakra-ui/react';
+import {
+  Text,
+  useToast,
+  Box,
+  TableContainer,
+  Table,
+  Thead,
+  Th,
+  Tr,
+  Tbody,
+  Heading
+} from '@chakra-ui/react';
+
+interface Props {
+  name: string;
+  email: string;
+  phone: string;
+  datetime: string;
+  description?: string;
+}
 
 export const VaccinationPage = () => {
-  const [curData, setCurData] = useState();
+  const [curData, setCurData] = useState<Props[]>([]);
   const toast = useToast();
 
   useEffect(() => {
@@ -17,7 +36,7 @@ export const VaccinationPage = () => {
 
     socket.onmessage = (event) => {
       const cur = JSON.parse(event.data)['bookings'];
-      setCurData(cur[cur.length - 1]);
+      setCurData(cur);
       toast({
         description: 'WIBU ROJAN',
         status: 'success',
@@ -41,7 +60,55 @@ export const VaccinationPage = () => {
 
   return (
     <Layout title="Vaccination Hospital" bg="/tohsaka.jpg">
-      <Box bg="#FFFFFFCC" borderRadius="lg" py={4} px={5}></Box>
+      <Box bg="#FFFFFFCC" borderRadius="lg" py={4} px={5}>
+        <Heading fontSize="lg" textAlign="center" mb={3}>
+          List Vaksinasi
+        </Heading>
+        <TableContainer>
+          <Table variant="simple">
+            <Thead>
+              <Tr>
+                <Th>
+                  <Text>Name</Text>
+                </Th>
+                <Th>
+                  <Text>Email</Text>
+                </Th>
+                <Th>
+                  <Text>Phone</Text>
+                </Th>
+                <Th>
+                  <Text>Date</Text>
+                </Th>
+                <Th>
+                  <Text>Description</Text>
+                </Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {curData?.map((item: Props, index: number) => (
+                <Tr key={index}>
+                  <Th>
+                    <Text>{item.name}</Text>
+                  </Th>
+                  <Th>
+                    <Text>{item.email}</Text>
+                  </Th>
+                  <Th>
+                    <Text>{item.phone}</Text>
+                  </Th>
+                  <Th>
+                    <Text>{item.datetime}</Text>
+                  </Th>
+                  <Th>
+                    <Text>{item.description}</Text>
+                  </Th>
+                </Tr>
+              ))}
+            </Tbody>
+          </Table>
+        </TableContainer>
+      </Box>
     </Layout>
   );
 };
